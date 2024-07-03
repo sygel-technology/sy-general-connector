6891147cbec639c9d37c31fe93c4cc65b667898d
manu <manuel.regidor@sygel.es>
2024-07-03 13:37:46 +0200

    [T-4718][ADD] ecommerce_connector_sii: Set the invoice as Simplified if the customer has no VAT

---
+++ ecommerce_connector_sii/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Ecommerce Connector - SII",
+    "summary": "SII values and procedures for ecommerce sale orders.",
+    "version": "14.0.1.0.0",
+    "category": "Custom",
+    "author": "Sygel",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "ecommerce_connector",
+        "l10n_es_aeat_sii_oca",
+    ],
+}

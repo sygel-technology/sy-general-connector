522ef6e9ee7d60a03e6914aa99b6ac045fdbd765
sygel <alberto.martinez@sygel.es>
2026-09-23 13:47:03 +0200

    [ADD] base_connector: base repo module + [REF] ecommerce_connector

---
+++ base_connector/models/__init__.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Valentin Vinagre <valentin.vinagre@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import ecommerce_connector
+from . import res_company
+from . import product_product
+from . import product_template
+from . import product_template_attribute_value
+from . import ecommerce_connector_call
+from . import ecommerce_connection
+from . import ecommerce_product_product
+from . import ecommerce_product_template
+from . import ecommerce_partner
+from . import res_partner
